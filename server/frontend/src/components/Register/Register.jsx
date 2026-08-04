347ef618f090a1ee3e4eaf6ca976ee09c78773ce
minhtb <minhtb1105@gmail.com>
2026-08-04 11:01:01 +0700

    Fix register placeholders and server output

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -22,19 +22,56 @@ export default function Register() {
     <form className="register-form" onSubmit={submitRegistration}>
       <h1>Sign-Up</h1>
       <label htmlFor="username">Username</label>
-      <input id="username" name="username" value={form.username} onChange={updateField} required />
+      <input
+        id="username"
+        name="username"
+        placeholder="Username"
+        value={form.username}
+        onChange={updateField}
+        required
+      />
 
       <label htmlFor="firstName">First Name</label>
-      <input id="firstName" name="firstName" value={form.firstName} onChange={updateField} required />
+      <input
+        id="firstName"
+        name="firstName"
+        placeholder="First Name"
+        value={form.firstName}
+        onChange={updateField}
+        required
+      />
 
       <label htmlFor="lastName">Last Name</label>
-      <input id="lastName" name="lastName" value={form.lastName} onChange={updateField} required />
+      <input
+        id="lastName"
+        name="lastName"
+        placeholder="Last Name"
+        value={form.lastName}
+        onChange={updateField}
+        required
+      />
 
       <label htmlFor="email">Email</label>
-      <input id="email" name="email" type="email" value={form.email} onChange={updateField} required />
+      <input
+        id="email"
+        name="email"
+        type="email"
+        placeholder="Email"
+        value={form.email}
+        onChange={updateField}
+        required
+      />
 
       <label htmlFor="password">Password</label>
-      <input id="password" name="password" type="password" value={form.password} onChange={updateField} required />
+      <input
+        id="password"
+        name="password"
+        type="password"
+        placeholder="Password"
+        value={form.password}
+        onChange={updateField}
+        required
+      />
 
       <button type="submit">Register</button>
     </form>
